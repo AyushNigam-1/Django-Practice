d23e92eb4cd28d47114a60ba48d5cf09218aff98
not-known-person <ayushnigam843@gmail.com>
2024-06-23 14:30:05 +0530

    Added Form Data Api Calling

--- web_project/views.py
+++ web_project/views.py
@@ -1,15 +1,23 @@
 from django.http import HttpResponse
 from django.shortcuts import render
 def homepage(request):
-    data={
-        "title":"wow",
-        "clist":[
-            {"name":"Ayush" , "age":"16"},
-            {"name":"Ayush","age":"18"},
-            {"name":"Ayush","age":"19"}
-            ]
-    }
-    return render(request,"index.html",data)
+    # data={
+    #     "title":"wow",
+    #     "clist":[
+    #         {"name":"Ayush" , "age":"16"},
+    #         {"name":"Ayush","age":"18"},
+    #         {"name":"Ayush","age":"19"}
+    #         ]
+    # }
+    print(request.GET['ipt'])
+    print(request.GET.get('ipt'))
+    # try:
+    #     ipt1 = request.GET['ipt']
+    #     ipt2 = request.GET['ipt2']
+    #     print("result" ,ipt1+ipt2)
+    # except:
+    #     pass
+    return render(request,"index.html")
 
 def about(request):
     return render(request,"about.html")
