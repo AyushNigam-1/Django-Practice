7a80631e3831dcb84441fbf60119dbdc702b2251
not-known-person <ayushnigam843@gmail.com>
2024-06-25 15:19:17 +0530

    Added Django Dynamic Form

--- web_project/views.py
+++ web_project/views.py
@@ -1,14 +1,20 @@
 from django.http import HttpResponse , HttpResponseRedirect
 from django.shortcuts import render , redirect
+from .forms import usersForm
 def homepage(request):
     url=""
+    fo = usersForm()
+    data = {
+    "output":fo    
+    }
+    print(data)
     if request.method == 'POST':
         n1 = int(request.POST.get("ipt1"))
         n2 = int(request.POST.get("ipt2"))
         res = n1+n2
         url = "/about?output={}".format(res)
         return HttpResponseRedirect(url)
-    return render(request , "index.html")
+    return render(request , "index.html" , data)
 
 def about(request):
     if request.method == 'GET':
