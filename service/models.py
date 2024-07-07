f1105a020348700ba94793cf2cbd91ec53a0c125
not-known-person <ayushnigam843@gmail.com>
2024-07-07 15:19:39 +0530

    Added Media Upload Support

--- service/models.py
+++ service/models.py
@@ -2,5 +2,6 @@ from django.db import models
 class Service(models.Model):
     service_icon=models.CharField(max_length=50)
     service_title=models.CharField(max_length=50)
+    icon = models.FileField(upload_to=",media/",max_length=250 ,null=True , default=None)
 
 
--- web_project/urls.py
+++ web_project/urls.py
@@ -17,6 +17,8 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 from web_project import views 
+from django.conf import settings
+from django.conf.urls.static import static
 urlpatterns = [
     path('',views.homepage, name="home"),
     path('admin/', admin.site.urls),
@@ -26,3 +28,6 @@ urlpatterns = [
     path("item/<int:id>",views.item,name="item"),
     path("form",views.userpage,name="form")
 ]
+
+if settings.DEBUG:
+    urlpatterns+=static(settings.MEDIA_URL , document_root = settings.MEDIA_ROOT)
