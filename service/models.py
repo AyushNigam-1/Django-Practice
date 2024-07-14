2c3eb4ca4ea50d4fb59ddffb384a32b8407c43e6
not-known-person <ayushnigam843@gmail.com>
2024-07-14 15:38:38 +0530

    Added Object Manager

--- service/models.py
+++ service/models.py
@@ -2,6 +2,11 @@ from django.db import models
 from django.contrib.auth.models import User
 from django.contrib.auth.models import AbstractBaseUser
 from .manager import UserManager
+
+class carManager(models.Manager):
+    def get_queryset(self):
+         return super().get_queryset().filter(is_deleted = False)
+
 class Service(models.Model):
     user = models.ForeignKey(User,on_delete=models.CASCADE,null = True,blank=True)
     service_icon=models.CharField(max_length=50)
@@ -23,6 +28,7 @@ class CustomUser(AbstractBaseUser):
 class Car(models.Model):
     car_name = models.CharField(max_length=500)
     speed = models.CharField(max_length=50)
+    is_deleted = models.BooleanField(default = False)
     
     def __str__(self) -> str:
         return self.car_name
@@ -30,5 +36,8 @@ class Car(models.Model):
     class Meta:
         # ordering = ['car']
         verbose_name = 'car'
-
+    
+    objects = carManager()
+    admin_objects = models.Manager()
+ 
 
